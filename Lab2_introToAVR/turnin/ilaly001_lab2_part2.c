cfd05bcdc64ce2deebb68b108d56d9b7058b247a
Inderpreet Laly <ilaly001@wch136-24.cs.ucr.edu>
2021-04-10 14:37:16 -0700

    Revised lab2

--- Lab2_introToAVR/turnin/ilaly001_lab2_part2.c
+++ Lab2_introToAVR/turnin/ilaly001_lab2_part2.c
@@ -13,34 +13,37 @@
 #endif
 
 int main(void) {
-    
-    DDRA = 0x00; PORTA = 0xFF;
+DDRA=0x00;PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
-     unsigned char tmpA = 0;
-    unsigned char cntaval = 0;
-
+    unsigned char tempA = 0x00;
+    unsigned char cntavail = 0;
     while (1) {
-	tmpA = PINA;
-	if (tmpA == 0x01) {
-	        cntaval +=1;
-                     PORTC = cntaval;
+	tempA = PINA;
+	if (tempA == 0x00) {
+ 		//4 Spots
+                cntavail = 0x04;
+		PORTC = cntavail;
 	}
-	if (tmpA == 0x02) {
-                  
-                       cntaval +=1;
-		PORTC = cntaval;	
+	else if (tempA == 0x01 || tempA == 0x02 || tempA == 0x04 || tempA == 0x08) {
+		//3 Spots
+                 cntavail = 0x03;
+                PORTC =cntavail;	
 	}
-	if ( tmpA == 0x04) {
-                    
-                     cntaval +=1;
-                    PORTC = cntaval;	
+	else if (tempA == 0x03 || tempA == 0x05 || tempA == 0x06 || tempA == 0x09 || tempA == 0x0A || tempA == 0x0C) {
+		//2 Spots
+                cntavail = 0x02;
+                PORTC = 0x02;
+	}
+	else if (tempA == 0x07 || tempA == 0x0B || tempA == 0x0D || tempA == 0x0E) {
+                  //1 Parking
+                   cntavail = 0x01;
+                   PORTC = 0x01;
         }
-	if(tmpA == 0x08){
-           cntaval +=1;
-           PORTC = cntaval;
-	 
-   }
-}
+	else {
+                  //No Parking
+ 		cntavail = 0x00;
+		PORTC = cntavail;
+	}
+    }
     return 1;
 }
-
