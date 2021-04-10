adab156553e76472e2b307f25687b0445f7090b3
Inderpreet Laly <ilaly001@wch136-24.cs.ucr.edu>
2021-04-10 12:32:23 -0700

    complete lab2.part2

---
+++ Lab2_introToAVR/turnin/ilaly001_lab2_part2.c
@@ -0,0 +1,44 @@
+/*	Author: ilaly001
+ *  Partner(s) Name: None
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+     unsigned char tmpA = 0;
+    unsigned char cntaval = 0;
+
+    while (1) {
+	tmpA = PINA;
+	if (tmpA == 0x01) {
+	        cntaval +=1;
+                     PORTC = cntaval;
+	}
+	if (tmpA == 0x02) {
+                  
+                       cntaval +=1;
+		PORTC = cntaval;	
+	}
+	if ( tmpA == 0x04) {
+                    
+                     cntaval +=1;
+                    PORTC = cntaval;	
+        }
+	if(tmpA == 0x08){
+           cntaval +=1;
+           PORTC = cntaval;
+	    }
+    return 1;
+}
+
