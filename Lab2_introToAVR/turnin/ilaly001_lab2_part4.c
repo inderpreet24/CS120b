fda9d8527157b23ec05c9bab7d1a5996ea302ba5
Inderpreet Laly <ilaly001@wch136-24.cs.ucr.edu>
2021-04-10 19:01:23 -0700

    Completed challenge Lab2

---
+++ Lab2_introToAVR/turnin/ilaly001_lab2_part4.c
@@ -0,0 +1,44 @@
+/*	Author: ilaly001
+ *  Partner(s) Name: None
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0xFF;
+    DDRC = 0x00; PORTC = 0xFF;
+    DDRD = 0xFF; PORTC =0x00;
+     unsigned char tmpA = 0;
+    unsigned char tmpB = 0;
+   unsigned char tmpC - 0;
+   unsigned char tmpD = 0;
+
+    while (1) {
+	tmpA = PINA;
+        tmpB = PINB;
+        tmpC = PINC;
+        tmpD = (tmpA + tmpB + tmpC) >>2;
+ 	tmpD = tempD & 0xFC;
+
+	if (tmpA - tmpC > 80 || tmpC - tmpA > 80) {
+	        tmpD = tmpD | 0x02;
+ 		PORTD = tmpD;
+	}
+	if (tmpA + tmpB + tmpC > 140) {
+                 tmpD = tmpD | 0x03;
+		PORTD= tmpD;	
+	}
+	    }
+    return 1;
+}
+
